62f2d3a4fb1bd846ce9d22f483159526f6cd7747
Ricardo Vela <aricav96@gmail.com>
2020-04-20 21:06:12 -0500

    Add first fragment
    This is just a checkpoint. Eventually we'll want this fragment to
    host the NavController and make a quick navigation graph

--- app/src/main/java/com/example/referenciapp/MainActivity.kt
+++ app/src/main/java/com/example/referenciapp/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.referenciapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.referenciapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
