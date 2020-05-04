4fb3064c48467e39c25810f9f3c01edae11117c7
Ricardo Vela <aricav96@gmail.com>
2020-05-03 22:18:31 -0500

    Add TabLayout to ReferenceMenuFragment
    Now we can list exercises in the tablayout. We still need to see how
    the navigation is supposed to be implemented.

--- app/src/main/java/com/example/referenciapp/ReferenceMenuFragment.kt
+++ app/src/main/java/com/example/referenciapp/ReferenceMenuFragment.kt
@@ -13,29 +13,39 @@ import com.example.referenciapp.databinding.FragmentReferenceMenuBinding
  * A simple [Fragment] subclass.
  */
 class ReferenceMenuFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding : FragmentReferenceMenuBinding = DataBindingUtil.inflate(
+        val binding: FragmentReferenceMenuBinding = DataBindingUtil.inflate(
             inflater,
             R.layout.fragment_reference_menu,
             container,
             false
         )
 
-        // Navigation to Exercise Fragment
-        // This is only for testing/MVP purposes. Eventually we'll add
-        // more exercise entries
-        binding.exerciseButton.setOnClickListener(
-            Navigation.
-                createNavigateOnClickListener(
-                    R.id.action_referenceMenuFragment_to_exerciseFragment
-                )
-        )
+        var tabLayout = binding.tabLayout
+        var viewPager = binding.viewPager
+        var supportFragmentManager = requireActivity().supportFragmentManager
 
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(PaperTabFragment(), "Papel")
+        adapter.addFragment(DigitalTabFragment(), "Digital")
+
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+        tabLayout.getTabAt(0)!!.setIcon(R.drawable.ic_description_black_24dp)
+        tabLayout.getTabAt(1)!!.setIcon(R.drawable.ic_desktop_mac_black_24dp)
+
+//        // Navigation to Exercise Fragment
+//        // This is only for testing/MVP purposes. Eventually we'll add
+//        // more exercise entries
+//        binding.exerciseButton.setOnClickListener(
+//            Navigation.
+//                createNavigateOnClickListener(
+//                    R.id.action_referenceMenuFragment_to_exerciseFragment
+//                )
+//        )
         return binding.root
     }
-
 }
