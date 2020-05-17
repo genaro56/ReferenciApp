6a35c0b1145818283c1e3a37eb6f3e3a924feac4
Ricardo Vela <aricav96@gmail.com>
2020-05-16 20:06:47 -0500

    Add Dao Skeleton

---
+++ app/src/main/java/com/example/referenciapp/database/ReferencesDao.kt
@@ -0,0 +1,12 @@
+package com.example.referenciapp.database
+
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Transaction
+
+@Dao
+interface ReferencesDao {
+    @Transaction
+    @Query("SELECT * FROM Book")
+    fun getBooksWithAuthor(): List<BookWithAuthors>
+}
