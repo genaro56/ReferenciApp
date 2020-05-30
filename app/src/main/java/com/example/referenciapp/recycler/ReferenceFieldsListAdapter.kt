8d2587a7da0967760738577f177d8eba5bd8ec44
Ricardo Vela <aricav96@gmail.com>
2020-05-30 12:37:30 -0500

    Add RecyclerView logic for displaying current print reference
    TODO:
    Figure out how to omit empty fields

--- app/src/main/java/com/example/referenciapp/recycler/ReferenceFieldsListAdapter.kt
+++ app/src/main/java/com/example/referenciapp/recycler/ReferenceFieldsListAdapter.kt
@@ -9,28 +9,38 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.referenciapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.reference_fields_view_holder.view.*
 
 class ReferenceFieldsListAdapter(
-    context: Context,
-    private val attributes: MutableList<String>
-    // val adapterOnClick: (Any) -> Unit
+    context: Context
 ) : RecyclerView.Adapter<ReferenceFieldsListAdapter.FieldViewHolder>() {
+
     private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var attributes = emptyList<String>()
 
     inner class FieldViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val fieldLabel = itemView.fieldLabel as TextView
+        val fieldValueTextView = itemView.fieldTextView as TextView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FieldViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val itemView = inflater.inflate(R.layout.reference_fields_view_holder, parent, false)
+        return FieldViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: FieldViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val current = attributes[position]
+
+        holder.fieldValueTextView.text = current
+    }
+
+    internal fun setAttributes(attrs: List<String>) {
+        this.attributes = attrs
+        notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return attributes.size
     }
 
 }
--- app/src/main/java/com/example/referenciapp/screens/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/example/referenciapp/screens/exercise/ExerciseFragment.kt
@@ -6,15 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.referenciapp.ExerciseViewModel
 import com.example.referenciapp.R
 import com.example.referenciapp.ReferenceMenuViewModel
+import com.example.referenciapp.database.PrintExercises
 import com.example.referenciapp.databinding.FragmentExerciseBinding
+import com.example.referenciapp.recycler.ReferenceFieldsListAdapter
 
 @Suppress("DEPRECATION")
 class ExerciseFragment : Fragment() {
 
-    private lateinit var viewModel: ReferenceMenuViewModel
+    private lateinit var appViewModel: ReferenceMenuViewModel
+    private lateinit var exerciseViewModel: ExerciseViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,11 +32,118 @@ class ExerciseFragment : Fragment() {
             container,
             false
         )
-        // Set the View Model to observe the selected exercise.
-        viewModel =
+
+        // Set the View Models to observe the selected exercise and update the recycler view
+        appViewModel =
             ViewModelProvider(requireNotNull(activity)).get(ReferenceMenuViewModel::class.java)
 
+        exerciseViewModel =
+            ViewModelProvider(requireNotNull(activity)).get(ExerciseViewModel::class.java)
+
+        val recyclerView = binding.referenceFieldsRecycler
+        val adapter = ReferenceFieldsListAdapter(requireContext())
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val currentExercise: Any
+        if(appViewModel.resourceType.value == 0) {
+            // It is a print exercise
+            currentExercise = appViewModel.currentPrintExercise.value!!
+            exerciseViewModel.setAttrs(
+                extractPrintFields(currentExercise)
+            )
+        }
+        else {
+            // It is a digital exercise
+            currentExercise = appViewModel.currentDigitalExercise
+
+
+        }
+
+        exerciseViewModel.attributes.observe(viewLifecycleOwner, Observer { attributes ->
+            attributes?.let { adapter.setAttributes(attributes) }
+        })
 
         return binding.root
+    }
+
+    private fun extractPrintFields(currentExercise: PrintExercises): List<String> {
+        var fields = emptyList<String>()
+
+        when (currentExercise.resourceType) {
+            1 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.editors,
+                    currentExercise.title,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher
+                )
+            }
+            2 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.editors,
+                    currentExercise.title,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher,
+                    currentExercise.chapterTitle,
+                    currentExercise.editors,
+                    currentExercise.pages
+                )
+            }
+            3 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.city,
+                    currentExercise.term,
+                    currentExercise.source,
+                    currentExercise.edition,
+                    currentExercise.publisher
+                )
+            }
+            4 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.title,
+                    currentExercise.city,
+                    currentExercise.source,
+                    currentExercise.country,
+                    currentExercise.publisher,
+                    currentExercise.chapterTitle,
+                    currentExercise.editors,
+                    currentExercise.volumeAndPages,
+                    currentExercise.term
+                )
+            }
+            5 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.date,
+                    currentExercise.title,
+                    currentExercise.newspaper,
+                    currentExercise.pages
+                )
+            }
+            6 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.title,
+                    currentExercise.journal,
+                    currentExercise.volume,
+                    currentExercise.issue,
+                    currentExercise.pages
+                )
+            }
+        }
+
+        return fields
     }
 }
