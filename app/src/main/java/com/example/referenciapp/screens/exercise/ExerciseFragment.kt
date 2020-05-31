773d7f2b9691eacebc89abbd1385449a6c373923
Ricardo Vela <aricav96@gmail.com>
2020-05-31 14:33:03 -0500

    Remake Type 2 Exercise Style
    Fixes weird constraints that made the type unusable upon phone
    orientation change

--- app/src/main/java/com/example/referenciapp/screens/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/example/referenciapp/screens/exercise/ExerciseFragment.kt
@@ -113,7 +113,7 @@ class ExerciseFragment : Fragment() {
             // It is a print exercise
             currentExercise = appViewModel.currentPrintExercise.value!!
             titleBarSetup(supportActionBar, currentExercise.id, currentExercise.description)
-            correctAnswer = extractPrintFields(currentExercise)
+            correctAnswer = ReferenceUtils.extractPrintFields(currentExercise)
             attrs = correctAnswer
 
             while(attrs == correctAnswer) {
@@ -128,7 +128,7 @@ class ExerciseFragment : Fragment() {
             // It is a digital exercise
             currentExercise = appViewModel.currentDigitalExercise.value!!
             titleBarSetup(supportActionBar, currentExercise.id, currentExercise.description)
-            correctAnswer = extractDigitalFields(currentExercise)
+            correctAnswer = ReferenceUtils.extractDigitalFields(currentExercise)
             attrs = correctAnswer
 
             while(attrs == correctAnswer) {
@@ -182,181 +182,5 @@ class ExerciseFragment : Fragment() {
                 }
                 .show()
         }
-    }
-
-    private fun extractDigitalFields(currentExercise: DigitalExercises): List<String> {
-        var fields = emptyList<String>()
-
-        when (currentExercise.resourceType) {
-            1 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    currentExercise.title,
-                    currentExercise.url
-                )
-            }
-            2 -> {
-                fields = listOf<String>(
-                    currentExercise.term,
-                    currentExercise.year,
-                    currentExercise.institution,
-                    currentExercise.edition,
-                    currentExercise.url
-                )
-            }
-            3 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    currentExercise.term,
-                    currentExercise.editor,
-                    if(currentExercise.hasEditor) currentExercise.editorString else "",
-                    currentExercise.source,
-                    currentExercise.edition,
-                    currentExercise.city,
-                    currentExercise.country,
-                    currentExercise.publisher,
-                    currentExercise.url
-                )
-            }
-            4 -> {
-                fields = listOf<String>(
-                    currentExercise.corp,
-                    currentExercise.year,
-                    currentExercise.title,
-                    currentExercise.edition,
-                    currentExercise.city,
-                    currentExercise.country,
-                    currentExercise.publisher
-                )
-            }
-            5 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    currentExercise.title,
-                    currentExercise.journal,
-                    currentExercise.issue,
-                    currentExercise.pages,
-                    currentExercise.url
-                )
-            }
-            6 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.date,
-                    currentExercise.title,
-                    currentExercise.blogtag,
-                    currentExercise.url
-                )
-            }
-            7 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.date,
-                    currentExercise.tweet,
-                    currentExercise.tweetTag,
-                    currentExercise.url
-                )
-            }
-            8 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.date,
-                    currentExercise.title,
-                    currentExercise.videoTag,
-                    currentExercise.url
-                )
-            }
-            9 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.date,
-                    currentExercise.title,
-                    currentExercise.imgTag,
-                    currentExercise.url
-                )
-            }
-        }
-
-        return fields.filter { x -> x != "" }
-    }
-
-    private fun extractPrintFields(currentExercise: PrintExercises): List<String> {
-        var fields = emptyList<String>()
-
-        when (currentExercise.resourceType) {
-            1 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    if (currentExercise.isEditor) currentExercise.editorString else "",
-                    currentExercise.title,
-                    currentExercise.city,
-                    currentExercise.country,
-                    currentExercise.publisher
-                )
-            }
-            2 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    currentExercise.chapterTitle,
-                    currentExercise.editors,
-                    currentExercise.title,
-                    currentExercise.pages,
-                    currentExercise.city,
-                    currentExercise.country,
-                    currentExercise.publisher
-                )
-            }
-            3 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    currentExercise.term,
-                    currentExercise.source,
-                    currentExercise.edition,
-                    currentExercise.city,
-                    currentExercise.country,
-                    currentExercise.publisher
-                )
-            }
-            4 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    currentExercise.term,
-                    currentExercise.source,
-                    currentExercise.volumeAndPages,
-                    currentExercise.city,
-                    currentExercise.country,
-                    currentExercise.publisher
-                )
-            }
-            5 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.date,
-                    currentExercise.title,
-                    currentExercise.newspaper,
-                    currentExercise.pages
-                )
-            }
-            6 -> {
-                fields = listOf<String>(
-                    currentExercise.authors,
-                    currentExercise.year,
-                    currentExercise.title,
-                    currentExercise.journal,
-                    currentExercise.volume,
-                    currentExercise.issue,
-                    currentExercise.pages
-                )
-            }
-        }
-
-        return fields.filter { x -> x != "" }
     }
 }
--- app/src/main/java/com/example/referenciapp/screens/exercise/ReferenceUtils.kt
+++ app/src/main/java/com/example/referenciapp/screens/exercise/ReferenceUtils.kt
@@ -1,5 +1,7 @@
 package com.example.referenciapp.screens.exercise
 
+import com.example.referenciapp.database.DigitalExercises
+import com.example.referenciapp.database.PrintExercises
 import java.util.*
 import kotlin.reflect.typeOf
 
@@ -18,5 +20,181 @@ object ReferenceUtils {
             list[j] = temp
         }
         return list
+    }
+
+    fun extractDigitalFields(currentExercise: DigitalExercises): List<String> {
+        var fields = emptyList<String>()
+
+        when (currentExercise.resourceType) {
+            1 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.title,
+                    currentExercise.url
+                )
+            }
+            2 -> {
+                fields = listOf<String>(
+                    currentExercise.term,
+                    currentExercise.year,
+                    currentExercise.institution,
+                    currentExercise.edition,
+                    currentExercise.url
+                )
+            }
+            3 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.term,
+                    currentExercise.editor,
+                    if(currentExercise.hasEditor) currentExercise.editorString else "",
+                    currentExercise.source,
+                    currentExercise.edition,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher,
+                    currentExercise.url
+                )
+            }
+            4 -> {
+                fields = listOf<String>(
+                    currentExercise.corp,
+                    currentExercise.year,
+                    currentExercise.title,
+                    currentExercise.edition,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher
+                )
+            }
+            5 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.title,
+                    currentExercise.journal,
+                    currentExercise.issue,
+                    currentExercise.pages,
+                    currentExercise.url
+                )
+            }
+            6 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.date,
+                    currentExercise.title,
+                    currentExercise.blogtag,
+                    currentExercise.url
+                )
+            }
+            7 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.date,
+                    currentExercise.tweet,
+                    currentExercise.tweetTag,
+                    currentExercise.url
+                )
+            }
+            8 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.date,
+                    currentExercise.title,
+                    currentExercise.videoTag,
+                    currentExercise.url
+                )
+            }
+            9 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.date,
+                    currentExercise.title,
+                    currentExercise.imgTag,
+                    currentExercise.url
+                )
+            }
+        }
+
+        return fields.filter { x -> x != "" }
+    }
+
+    fun extractPrintFields(currentExercise: PrintExercises): List<String> {
+        var fields = emptyList<String>()
+
+        when (currentExercise.resourceType) {
+            1 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    if (currentExercise.isEditor) currentExercise.editorString else "",
+                    currentExercise.title,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher
+                )
+            }
+            2 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.chapterTitle,
+                    currentExercise.editors,
+                    currentExercise.title,
+                    currentExercise.pages,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher
+                )
+            }
+            3 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.term,
+                    currentExercise.source,
+                    currentExercise.edition,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher
+                )
+            }
+            4 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.term,
+                    currentExercise.source,
+                    currentExercise.volumeAndPages,
+                    currentExercise.city,
+                    currentExercise.country,
+                    currentExercise.publisher
+                )
+            }
+            5 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.date,
+                    currentExercise.title,
+                    currentExercise.newspaper,
+                    currentExercise.pages
+                )
+            }
+            6 -> {
+                fields = listOf<String>(
+                    currentExercise.authors,
+                    currentExercise.year,
+                    currentExercise.title,
+                    currentExercise.journal,
+                    currentExercise.volume,
+                    currentExercise.issue,
+                    currentExercise.pages
+                )
+            }
+        }
+
+        return fields.filter { x -> x != "" }
     }
 }
